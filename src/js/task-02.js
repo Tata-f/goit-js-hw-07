2e04fa11f26025c4b37a9272a60442fffc46ed11
Tata-f <75538150+Tata-f@users.noreply.github.com>
2021-05-16 01:08:22 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,20 +1,17 @@
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
-  
-const ingredientsEl = document.querySelector('#ingredients')
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
 
+const ingredientsEl = document.querySelector("#ingredients");
 
-const elements = ingredients.map(element => {
-const liEl = document.createElement('li');
-liEl.textContent = element;
-return liEl;
-
+const elements = ingredients.map((element) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = element;
+  return liEl;
 });
-
-ingredientsEl.append(...elements)
+ingredientsEl.append(...elements);
