14bfccf4388717e811f9d84355dac8defc2afc48
Tata-f <75538150+Tata-f@users.noreply.github.com>
2021-05-16 16:37:05 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,3 +3,24 @@
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const counterEl = document.querySelectorAll("#counter button");
+const counterValue = document.querySelector("#value");
+
+const incrementBtn = counterEl[0];
+const decrementBtn = counterEl[1];
+
+incrementBtn.addEventListener("click", onIncrementBtnClick);
+decrementBtn.addEventListener("click", onDecrementBtnClick);
+
+let counter = 0;
+
+function onIncrementBtnClick() {
+  counter -= 1;
+  counterValue.textContent = `${counter}`;
+}
+
+function onDecrementBtnClick() {
+  counter += 1;
+  counterValue.textContent = `${counter}`;
+}
